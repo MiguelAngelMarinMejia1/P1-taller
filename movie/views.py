93da8c7b23365fdc2296c97eefee5bb7ffe93300
MiguelAngelMarinMejia1 <mamarinm2@eafit.edu.co>
2026-07-28 11:42:25 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse("<h1>Welcome to Home Page</h1>")
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Miguel Marín'})
+    return render(request, 'home.html', {'name': 'Miguel Angel Marín Mejía'})
 
 def about(request):
     return HttpResponse("<h1>Welcome to About Page</h1>")
